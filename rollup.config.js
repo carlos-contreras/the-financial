884389f5d6b9a248ad26da2a78c5e5198c665a81
Carlos Contreras <carlos@thoroughcare.net>
2019-11-21 00:06:13 -0500

    Add minimist package and rollup plugins

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,6 @@
 import json from 'rollup-plugin-json';
+import commonjs from 'rollup-plugin-commonjs';
+import nodeResolve from 'rollup-plugin-node-resolve';
 
 export default {
   input: 'src/main.js',
@@ -8,5 +10,16 @@ export default {
     banner: '#!/usr/bin/env node',
     footer: '/* follow me on Twitter! @carlos-contreras */'
   },
-  plugins: [ json() ]
+  plugins: [
+    json(),
+    nodeResolve({
+      // the fields to scan in a package.json to determine the entry point
+      // if this list contains "browser", overrides specified in "pkg.browser"
+      // will be used
+      mainFields: ['module', 'main', 'index.js'], // Default: ['module', 'main']
+    }),
+    commonjs({
+      include: 'node_modules/**',
+    })
+  ]
 };
--- src/foo.js
+++ src/foo.js
@@ -1,1 +1,5 @@
-export default 'hello world! from ES6';
+import minimist from 'minimist'
+
+const args = minimist(process.argv.slice(2))
+
+export default `hello world! from ES6, this are the options passed: ${JSON.stringify(args)}`
